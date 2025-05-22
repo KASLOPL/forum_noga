323dfc664b148676ee318d936696f96a64151e2f
Kaslo <kasjan.bojko.gdansk@gmail.com>
2025-05-22 15:45:47 +0200

    yes

--- src/index.js
+++ src/index.js
@@ -2,27 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
- HEAD
-import Logowanie from './logowanie';
-import Rejestracja from './rejestracja';
-import Main from './main';
-import AddQuestion from './addquestion';
-import Zakladki from './zakładki';
-import Settings from './settings';
-
-import Logowanie from './pages/log_in/logowanie.js';
-import './pages/log_in/logowanie.css';
-
+import Logowanie from './pages/log_in/logowanie';
 import Rejestracja from './pages/sing_up/rejestracja.js';
-
 import Main from './pages/main/main.js';
-
 import AddQuestion from './pages/add_question/addquestion.js';
+import Zakladki from './pages/bookmarks/zakładki';
+import Settings from './pages/settings/settings.js';
+import './pages/log_in/logowanie.css';
 import './pages/add_question/addquestion.css';
-
-import Zakladki from './pages/bookmarks/zakładki.js';
-
- ee18ddfc9b55b7c1f88ec5c7a5fb6eb8732207a3
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
