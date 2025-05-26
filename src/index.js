ad69fd00fb89a04c7bd9a4f8a61a923faeca68b9
Minciak-z <nikolanowak2007n@gmail.com>
2025-05-26 23:39:44 +0200

    json

--- src/index.js
+++ src/index.js
@@ -2,25 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
+
 import Logowanie from './pages/log_in/logowanie.js';
 import './pages/log_in/logowanie.css';
+
 import Main from './pages/main/main.js';
-<<<<<<< HEAD
 import './pages/main/main.css';
-=======
->>>>>>> 0d974e7f4b79bab9948f7329d322713f68d6f32b
+
 import AddQuestion from './pages/add_question/addquestion.js';
-import Zakladki from './pages/bookmarks/zakładki';
-import Settings from './pages/settings/settings.js';
-import './pages/log_in/logowanie.css';
 import './pages/add_question/addquestion.css';
-<<<<<<< HEAD
-import Zakladki from './pages/bookmarks/zakładki.js';
+
+import Zakladki from './pages/bookmarks/zakładki';  
+import Settings from './pages/settings/settings.js';
 import Help from './pages/help/help.js';
-
-
-=======
->>>>>>> 0d974e7f4b79bab9948f7329d322713f68d6f32b
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
