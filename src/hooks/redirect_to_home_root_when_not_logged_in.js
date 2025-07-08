bb6e69b056bcf71001694a6669b03f798c407f59
maciej.kuster <maciej.kuster@dynatrace.com>
2025-07-08 12:29:43 +0200

    use navigate for redirection when not logged in

--- src/hooks/redirect_to_home_root_when_not_logged_in.js
+++ src/hooks/redirect_to_home_root_when_not_logged_in.js
@@ -1,11 +1,13 @@
 import {useEffect} from "react";
+import {useNavigate} from "react-router-dom";
 
 export const useRedirectToHomeRootWhenNotLoggedIn = () => {
+  const navigate = useNavigate();
   const loggedIn = localStorage.getItem('isLoggedIn');
 
   useEffect(() => {
     if (!loggedIn) {
-      window.location.href = '/';
+      navigate('/');
     }
   }, [loggedIn]);
 }
