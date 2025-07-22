e83b90eda4213ae60d230aa3bdae9a91c29dd14b
Mariia Osypenko <mardoffmaria@gmail.com>
2025-07-22 13:00:02 +0200

    ponownie naprawiony index po porzednio nie dzialalo

--- src/index.js
+++ src/index.js
@@ -20,7 +20,6 @@ import QuestionDetail from './pages/answ_question/answer_q';
 import MyQuestions from './pages/my_questions/my_questions.js';
 import Search from './pages/search/search.js';
 import ResetPassword from './pages/reset_password/ResetPassword.js';
-import logout from './hooks/logout.js';
 import { LogoutProvider } from './hooks/logout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -32,16 +31,14 @@ root.render(
           <Route path="/" element={<LogoutProvider><Logowanie /></LogoutProvider>} />
           <Route path="/logowanie" element={<LogoutProvider><Logowanie /></LogoutProvider>} />
           <Route path="/reset_password" element={<ResetPassword />} />
-          <Route element={<LogoutProvider><Routes>
-            <Route path="/main" element={<Main />} />
-            <Route path="/addquestion" element={<AddQuestion />} />
-            <Route path="/zakładki" element={<Zakladki />} />
-            <Route path="/help" element={<Help />} />
-            <Route path='/profile' element={<Profile />}></Route>
-            <Route path="/answer_q/:id" element={<QuestionDetail />} />
-            <Route path='/my_questions' element={<MyQuestions />} />
-            <Route path='/search' element={<Search />} />
-          </Routes></LogoutProvider>} />
+          <Route path="/main" element={<LogoutProvider><Main /></LogoutProvider>} />
+          <Route path="/addquestion" element={<LogoutProvider><AddQuestion /></LogoutProvider>} />
+          <Route path="/zakładki" element={<LogoutProvider><Zakladki /></LogoutProvider>} />
+          <Route path="/help" element={<LogoutProvider><Help /></LogoutProvider>} />
+          <Route path="/profile" element={<LogoutProvider><Profile /></LogoutProvider>} />
+          <Route path="/answer_q/:id" element={<LogoutProvider><QuestionDetail /></LogoutProvider>} />
+          <Route path="/my_questions" element={<LogoutProvider><MyQuestions /></LogoutProvider>} />
+          <Route path="/search" element={<LogoutProvider><Search /></LogoutProvider>} />
         </Routes>
       </UserProvider>
     </BrowserRouter>
